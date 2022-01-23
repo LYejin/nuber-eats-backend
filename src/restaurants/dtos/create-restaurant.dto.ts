aa1565d6bf1eb2709a633f2703680afd7553da89
unknown <vgg789@naver.com>
2022-01-24 01:27:42 +0900

    backend

---
+++ src/restaurants/dtos/create-restaurant.dto.ts
@@ -0,0 +1,19 @@
+import { Field, InputType, ObjectType, PickType, Int } from '@nestjs/graphql';
+import { CoreOutput } from 'src/common/dtos/output.dto';
+import { Restaurant } from '../entities/restaurant.entity';
+
+@InputType()
+export class CreateRestaurantInput extends PickType(Restaurant, [
+  'name',
+  'coverImg',
+  'address',
+]) {
+  @Field(type => String)
+  categoryName: string;
+}
+
+@ObjectType()
+export class CreateRestaurantOutput extends CoreOutput {
+  @Field(type => Int)
+  restaurantId?: number;
+}
---
+++ src/uploads/uploads.controller.ts
@@ -0,0 +1,39 @@
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import * as AWS from 'aws-sdk';
+
+const BUCKET_NAME = 'allkit12';
+
+@Controller('uploads')
+export class UploadsController {
+  @Post('')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(@UploadedFile() file) {
+    AWS.config.update({
+      credentials: {
+        accessKeyId: 'AKIAQHZN36IBWR47WDUG',
+        secretAccessKey: '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',
+      },
+    });
+    try {
+      const objectName = `${Date.now() + file.originalname}`;
+      await new AWS.S3()
+        .putObject({
+          Body: file.buffer,
+          Bucket: BUCKET_NAME,
+          Key: objectName,
+          ACL: 'public-read',
+        })
+        .promise();
+      const url = `https://${BUCKET_NAME}.s3.amazonaws.com/${objectName}`;
+      return { url };
+    } catch (e) {
+      return null;
+    }
+  }
+}
